3ca8bf3580260e55dadd8949b328a0ff4761e862
Maliyah Raevyn <QphoriaTech@gmail.com>
2024-01-08 19:49:11 -0600

    image upload test fix

--- components/views/Browse.js
+++ components/views/Browse.js
@@ -1,13 +1,13 @@
 import html from "html-literal";
 
-import flyingDog from "../../assets/img/flyingDog.gif";
+import flyingDog from "../../assets/img/flyingDog.png";
 
 export default () => html`
   <section id="browse-content">
     <h2>Browse Artist Content</h2>
     <p>Explore a diverse range of artistic styles and creations.</p>
     <div id="Gallery">
-      <img < />
+      <img src=${flyingDog} />
     </div>
   </section>
 `;
