dec85ba1466cb7b048fb55b88ca79c58ef8b07a7
Maliyah Raevyn <QphoriaTech@gmail.com>
2024-01-08 19:33:20 -0600

    started building media gallery

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,8 +1,11 @@
 import html from "html-literal";
 
+import ColorStatic from "../../assets/img/ColorStatic.gif";
+
 export default state => html`
   <section id="home">
     <h1>Welcome to Artunia</h1>
+    <img src=${ColorStatic} alt="ColorfulStatic" id="ColorfulStatic" />
 
     <h3>
       The weather in ${state.weather.city} is ${state.weather.description}.
